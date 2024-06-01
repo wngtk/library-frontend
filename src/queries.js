04979c2c87b4858596cec46ea75c95f53b31901f
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-06-01 13:00:04 +0800

    8.10: Adding a book

--- src/queries.js
+++ src/queries.js
@@ -19,3 +19,14 @@ query AllBooks {
   }
 }
 `
+
+export const ADD_BOOK = gql`
+mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+  addBook(title: $title, author: $author, published: $published, genres: $genres) {
+    title
+    published
+    author
+    genres
+  }
+}
+`
