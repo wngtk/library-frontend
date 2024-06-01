2652c61a2d38ff2a55629a66cb71a36cb7292fee
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-06-01 12:46:59 +0800

    8.8: Authors view

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -1,8 +1,14 @@
+import { useQuery } from "@apollo/client"
+import { ALL_AUTHORS } from "../queries"
+
 const Authors = (props) => {
-  if (!props.show) {
+  const result = useQuery(ALL_AUTHORS)
+  
+  if (!props.show || result.loading) {
     return null
   }
-  const authors = []
+
+  const authors = result.data.allAuthors
 
   return (
     <div>
